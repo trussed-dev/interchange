31b5283e24d2e8b6a5e1d3f98c1a833c9aae7fb4
nickray <nickray@users.noreply.github.com>
2021-02-11 20:35:45 +0000

    deploy: f6ee324c6bd1c8926dd4235ab3f6404dfd039a28

--- implementors/core/marker/trait.Unpin.js
+++ implementors/core/marker/trait.Unpin.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["interchange"] = [{"text":"impl Unpin for State","synthetic":true,"types":[]},{"text":"impl&lt;I&gt; Unpin for Requester&lt;I&gt;","synthetic":true,"types":[]},{"text":"impl&lt;I&gt; Unpin for Responder&lt;I&gt;","synthetic":true,"types":[]}];
+implementors["interchange"] = [{"text":"impl&lt;I&gt; Unpin for Requester&lt;I&gt;","synthetic":true,"types":[]},{"text":"impl&lt;I&gt; Unpin for Responder&lt;I&gt;","synthetic":true,"types":[]},{"text":"impl Unpin for State","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
