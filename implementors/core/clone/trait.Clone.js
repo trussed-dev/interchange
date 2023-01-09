efe8cf2c84f58db8149de9b9941d5a4e553d78de
daringer <daringer@users.noreply.github.com>
2023-01-09 21:00:57 +0000

    deploy: 8c986d84577ab5d374c9c85d2d44b246c0fdc840

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["interchange"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>","synthetic":false,"types":["interchange::State"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"interchange":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
