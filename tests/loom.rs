40df05673c7d845f754a8726069071ed96d6cd04
Sosthène Guédon <sosthene@nitrokey.com>
2023-05-16 09:51:21 +0200

    Add missing branch check

--- tests/loom.rs
+++ tests/loom.rs
@@ -10,10 +10,10 @@ use interchange::{Channel, Requester, Responder};
 use std::sync::atomic::Ordering::Acquire;
 use std::sync::atomic::{AtomicBool, Ordering::Release};
 
-static BRANCHES_USED: [AtomicBool; 8] = {
+static BRANCHES_USED: [AtomicBool; 11] = {
     #[allow(clippy::declare_interior_mutable_const)]
     const ATOMIC_BOOL_INIT: AtomicBool = AtomicBool::new(false);
-    [ATOMIC_BOOL_INIT; 8]
+    [ATOMIC_BOOL_INIT; 11]
 };
 
 #[cfg(loom)]
@@ -51,48 +51,60 @@ fn test_function() {
 
 fn requester_thread(mut requester: Requester<'static, u64, u64>) -> Option<()> {
     requester.request(53).unwrap();
-    requester.with_response(|r| assert_eq!(*r, 63)).ok()?;
-    requester.with_response(|r| assert_eq!(*r, 63)).ok()?;
+    requester
+        .with_response(|r| {
+            BRANCHES_USED[0].store(true, Release);
+            assert_eq!(*r, 63)
+        })
+        .ok()
+        .or_else(|| {
+            BRANCHES_USED[1].store(true, Release);
+            None
+        })?;
+    requester.with_response(|r| assert_eq!(*r, 63)).unwrap();
     requester.take_response().unwrap();
     requester.with_request_mut(|r| *r = 51).unwrap();
     requester.send_request().unwrap();
     thread::yield_now();
     match requester.cancel() {
-        Ok(Some(51) | None) => BRANCHES_USED[0].store(true, Release),
+        Ok(Some(51) | None) => BRANCHES_USED[2].store(true, Release),
         Ok(_) => panic!("Invalid state"),
         Err(_) => {
-            BRANCHES_USED[1].store(true, Release);
+            BRANCHES_USED[3].store(true, Release);
             match requester.take_response() {
                 Some(i) => {
                     assert_eq!(i, 79);
-                    BRANCHES_USED[6].store(true, Release);
+                    BRANCHES_USED[4].store(true, Release);
                 }
-                None => BRANCHES_USED[7].store(true, Release),
+                None => BRANCHES_USED[5].store(true, Release),
             }
         }
     }
-    BRANCHES_USED[4].store(true, Release);
+    BRANCHES_USED[6].store(true, Release);
     None
 }
 
 fn responder_thread(mut responder: Responder<'static, u64, u64>) -> Option<()> {
-    let req = responder.take_request()?;
+    let req = responder.take_request().or_else(|| {
+        BRANCHES_USED[7].store(true, Release);
+        None
+    })?;
     assert_eq!(req, 53);
     responder.respond(req + 10).unwrap();
     thread::yield_now();
     responder
         .with_request(|r| {
-            BRANCHES_USED[2].store(true, Release);
+            BRANCHES_USED[8].store(true, Release);
             assert_eq!(*r, 51)
         })
         .map(|_| assert!(responder.with_request(|_| {}).is_err()))
         .or_else(|_| {
-            BRANCHES_USED[3].store(true, Release);
+            BRANCHES_USED[9].store(true, Release);
             responder.acknowledge_cancel()
         })
         .ok()?;
     responder.with_response_mut(|r| *r = 79).ok();
     responder.send_response().ok();
-    BRANCHES_USED[5].store(true, Release);
+    BRANCHES_USED[10].store(true, Release);
     None
 }
