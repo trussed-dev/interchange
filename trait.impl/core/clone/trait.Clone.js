7b7021a5d2c9e331b6fbe26d8ea809a90dfc9865
robin-nitrokey <robin-nitrokey@users.noreply.github.com>
2025-01-14 13:47:11 +0000

    deploy: 4a0eb996710face821ee7c643462c75109fc92db

--- trait.impl/core/clone/trait.Clone.js
+++ trait.impl/core/clone/trait.Clone.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["interchange",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"interchange/struct.Error.html\" title=\"struct interchange::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["interchange",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"interchange/struct.Error.html\" title=\"struct interchange::Error\">Error</a>"],["impl&lt;Rq, Rp&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"interchange/struct.InterchangeRef.html\" title=\"struct interchange::InterchangeRef\">InterchangeRef</a>&lt;'_, Rq, Rp&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[515]}
+//{"start":57,"fragment_lengths":[826]}
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["interchange",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"interchange/struct.Error.html\" title=\"struct interchange::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["interchange",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"interchange/enum.State.html\" title=\"enum interchange::State\">State</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"interchange/struct.Error.html\" title=\"struct interchange::Error\">Error</a>"],["impl&lt;Rq, Rp&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"interchange/struct.InterchangeRef.html\" title=\"struct interchange::InterchangeRef\">InterchangeRef</a>&lt;'_, Rq, Rp&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[513]}
+//{"start":57,"fragment_lengths":[823]}
